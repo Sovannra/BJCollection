5ee610efc152a7c88451e84f40ceb7a1df902a77
Sovannra <sovannrakong@gmail.com>
2021-12-11 09:32:53 +0700

    Initial commit

---
+++ Example/BJCollection/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BJCollection
+//
+//  Created by Sovannra on 12/11/2021.
+//  Copyright (c) 2021 Sovannra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
